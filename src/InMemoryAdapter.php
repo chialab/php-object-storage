4cb57dd9c8b934ddf4c4cc19a5496e7f4faa3b9d
Paolo Cuffiani <paolo.cuffiani@chialab.it>
2022-05-25 23:19:23 +0200

    feat: add in-memory adapter

---
+++ src/InMemoryAdapter.php
@@ -0,0 +1,194 @@
+<?php
+declare(strict_types=1);
+
+namespace Chialab\ObjectStorage;
+
+use Chialab\ObjectStorage\Exception\BadDataException;
+use Chialab\ObjectStorage\Exception\ObjectNotFoundException;
+use Chialab\ObjectStorage\Exception\StorageException;
+use Chialab\ObjectStorage\Utils\Promise;
+use Chialab\ObjectStorage\Utils\Stream;
+use GuzzleHttp\Promise\PromiseInterface;
+use GuzzleHttp\Psr7\Stream as PsrStream;
+use Webmozart\Assert\Assert;
+
+/**
+ * In-memory object storage, for testing purposes.
+ */
+class InMemoryAdapter implements MultipartUploadInterface
+{
+    /**
+     * In-memory storage.
+     *
+     * @var array<string, \Chialab\ObjectStorage\FileObject>
+     */
+    protected array $storage = [];
+
+    /**
+     * Multipart uploads.
+     *
+     * @var array<string, array{\Chialab\ObjectStorage\FileObject, array<int, \Chialab\ObjectStorage\FilePart>}>
+     */
+    protected array $multipart = [];
+
+    /**
+     * Get a copy of a file object.
+     *
+     * @param \Chialab\ObjectStorage\FileObject $object File object to copy.
+     * @return \Chialab\ObjectStorage\FileObject
+     */
+    protected static function copyObject(FileObject $object): FileObject
+    {
+        $stream = null;
+        if ($object->data !== null) {
+            $copy = Stream::newTemporaryStream();
+            Stream::psrCopyToStream($object->data, $copy);
+            rewind($copy);
+            $stream = new PsrStream($copy);
+        }
+
+        return new FileObject($object->key, $stream, $object->metadata);
+    }
+
+    /**
+     * Adapter constructor.
+     *
+     * @param string $baseUrl Base URL.
+     */
+    public function __construct(protected readonly string $baseUrl)
+    {
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function url(string $key): string
+    {
+        return $this->baseUrl . $key;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function has(string $key): PromiseInterface
+    {
+        return Promise::async(fn (): bool => isset($this->storage[$key]));
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function get(string $key): PromiseInterface
+    {
+        return Promise::async(fn (): FileObject => static::copyObject(
+            $this->storage[$key] ?? throw new ObjectNotFoundException(sprintf('Object not found: %s', $key))
+        ));
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function put(FileObject $object): PromiseInterface
+    {
+        return Promise::async(function () use ($object): void {
+            if ($object->data === null) {
+                throw new BadDataException('Missing object data');
+            }
+
+            $this->storage[$object->key] = static::copyObject($object);
+        });
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function delete(string $key): PromiseInterface
+    {
+        return Promise::async(function () use ($key): void {
+            unset($this->storage[$key]);
+        });
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function multipartInit(FileObject $object): PromiseInterface
+    {
+        return Promise::async(function () use ($object): string {
+            $token = bin2hex(random_bytes(32));
+            $this->multipart[$token] = [$object, []];
+
+            return $token;
+        });
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function multipartUpload(FileObject $object, string $token, FilePart $part): PromiseInterface
+    {
+        Assert::positiveInteger($part->part);
+
+        return Promise::async(function () use ($object, $token, $part): string {
+            if (!isset($this->multipart[$token]) || $this->multipart[$token][0]->key !== $object->key) {
+                throw new BadDataException(sprintf('Multipart upload not initialized: %s', $token));
+            }
+
+            $data = $part->data ?? throw new BadDataException('Missing part data');
+            $copy = Stream::newTemporaryStream();
+            Stream::psrCopyToStream($data, $copy);
+            rewind($copy);
+            $hash = Stream::hash($copy, 'sha256');
+            rewind($copy);
+
+            $this->multipart[$token][1][$part->part] = new FilePart($part->part, new PsrStream($copy), $hash, $part->metadata);
+
+            return $hash;
+        });
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function multipartFinalize(FileObject $object, string $token, FilePart ...$parts): PromiseInterface
+    {
+        return Promise::async(function () use ($object, $token, $parts): void {
+            $initialObject = $this->multipart[$token][0] ?? throw new BadDataException(sprintf('Multipart upload not initialized: %s', $token));
+            if ($initialObject->key !== $object->key) {
+                throw new BadDataException(sprintf('Multipart upload not initialized: %s', $token));
+            }
+
+            $dest = Stream::newTemporaryStream();
+            $partNo = 0;
+            foreach ($parts as $part) {
+                if ($part->part <= $partNo) {
+                    throw new BadDataException('Parts must be sorted monotonically');
+                }
+
+                $uploaded = $this->multipart[$token][1][$part->part] ?? throw new BadDataException(sprintf('Part not uploaded: %d', $part->part));
+                if ($uploaded->hash !== $part->hash) {
+                    throw new BadDataException(sprintf('Hash mismatch for part %d', $part->part));
+                }
+
+                $data = $uploaded->data ?? throw new StorageException('Missing part data');
+                Stream::psrCopyToStream($data, $dest);
+
+                $partNo = $part->part;
+            }
+            rewind($dest);
+
+            $this->storage[$object->key] = new FileObject($object->key, new PsrStream($dest), $initialObject->metadata ?: $object->metadata);
+            unset($this->multipart[$token]);
+        });
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function multipartAbort(FileObject $object, string $token): PromiseInterface
+    {
+        return Promise::async(function () use ($token): void {
+            unset($this->multipart[$token]);
+        });
+    }
+}
---
+++ tests/TestCase/InMemoryAdapterTest.php
@@ -0,0 +1,325 @@
+<?php
+declare(strict_types=1);
+
+namespace Chialab\ObjectStorage\Test\TestCase;
+
+use Chialab\ObjectStorage\Exception\BadDataException;
+use Chialab\ObjectStorage\Exception\ObjectNotFoundException;
+use Chialab\ObjectStorage\FileObject;
+use Chialab\ObjectStorage\FilePart;
+use Chialab\ObjectStorage\InMemoryAdapter;
+use GuzzleHttp\Psr7\Stream;
+use PHPUnit\Framework\Constraint\Exception;
+use PHPUnit\Framework\Constraint\ExceptionCode;
+use PHPUnit\Framework\Constraint\ExceptionMessage;
+use PHPUnit\Framework\TestCase;
+use Throwable;
+
+/**
+ * {@see \Chialab\ObjectStorage\InMemoryAdapter} Test Case
+ *
+ * @coversDefaultClass \Chialab\ObjectStorage\InMemoryAdapter
+ * @uses \Chialab\ObjectStorage\Utils\Promise
+ * @uses \Chialab\ObjectStorage\Utils\Stream
+ * @uses \Chialab\ObjectStorage\FileObject
+ * @uses \Chialab\ObjectStorage\FilePart
+ * @uses \Chialab\ObjectStorage\InMemoryAdapter::__construct()
+ */
+class InMemoryAdapterTest extends TestCase
+{
+    use FilesystemUtilsTrait;
+
+    /**
+     * Test subject.
+     *
+     * @var \Chialab\ObjectStorage\InMemoryAdapter
+     */
+    protected InMemoryAdapter $adapter;
+
+    /**
+     * @inheritDoc
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->adapter = new InMemoryAdapter('https://static.example.com/');
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected function tearDown(): void
+    {
+        unset($this->adapter);
+
+        parent::tearDown();
+    }
+
+    /**
+     * Assert that a callable throws an exception.
+     *
+     * @param \Throwable $expected Expected exception.
+     * @param callable(): mixed $callback Function that is expected to throw.
+     * @return void
+     */
+    protected static function assertThrows(Throwable $expected, callable $callback): void
+    {
+        try {
+            $callback();
+        } catch (Throwable $e) {
+            static::assertThat($e, new Exception(get_class($expected)));
+            static::assertThat($e, new ExceptionMessage($expected->getMessage()));
+            static::assertThat($e, new ExceptionCode($expected->getCode()));
+
+            return;
+        }
+
+        static::assertThat(null, new Exception(get_class($expected)));
+    }
+
+    /**
+     * Test {@see InMemoryAdapter::url()} method.
+     *
+     * @return void
+     * @covers ::url()
+     */
+    public function testUrl(): void
+    {
+        $expected = 'https://static.example.com/my-file.txt';
+        $actual = $this->adapter->url('my-file.txt');
+
+        static::assertSame($expected, $actual);
+    }
+
+    /**
+     * Test {@see InMemoryAdapter::has()}, {@see InMemoryAdapter::get()}, {@see InMemoryAdapter::put()}
+     * and {@see InMemoryAdapter::delete()} methods.
+     *
+     * @return void
+     * @covers ::has()
+     * @covers ::get()
+     * @covers ::put()
+     * @covers ::delete()
+     * @covers ::copyObject()
+     */
+    public function testCRUD(): void
+    {
+        static::assertFalse($this->adapter->has('example')->wait());
+        static::assertThrows(
+            new ObjectNotFoundException('Object not found: example'),
+            fn () => $this->adapter->get('example')->wait(),
+        );
+        $this->adapter->delete('example')->wait(); // This should not throw any exception.
+
+        static::assertThrows(
+            new BadDataException('Missing object data'),
+            fn () => $this->adapter->put(new FileObject('example', null))->wait(),
+        );
+
+        $object = new FileObject('example', new Stream(static::asStream('hello world!')));
+        $this->adapter->put($object)->wait();
+
+        static::assertTrue($this->adapter->has('example')->wait());
+        $stored = $this->adapter->get('example')->wait();
+        static::assertNotSame($object, $stored);
+        static::assertSame('example', $stored->key);
+        static::assertNotSame($object->data, $stored->data);
+        static::assertSame('hello world!', $stored->data?->getContents());
+
+        $this->adapter->delete('example')->wait();
+        static::assertFalse($this->adapter->has('example')->wait());
+    }
+
+    /**
+     * Test {@see InMemoryAdapter::multipartInit()}, {@see InMemoryAdapter::multipartUpload()}
+     * and {@see InMemoryAdapter::multipartFinalize()} with a successful flow.
+     *
+     * @return void
+     * @covers ::multipartInit()
+     * @covers ::multipartUpload()
+     * @covers ::multipartFinalize()
+     * @uses \Chialab\ObjectStorage\InMemoryAdapter::has()
+     * @uses \Chialab\ObjectStorage\InMemoryAdapter::get()
+     * @uses \Chialab\ObjectStorage\InMemoryAdapter::copyObject()
+     */
+    public function testMultipartUpload(): void
+    {
+        static::assertFalse($this->adapter->has('example')->wait());
+
+        $object = new FileObject('example', null);
+        $token = $this->adapter->multipartInit($object)->wait();
+        static::assertIsString($token);
+        static::assertNotEmpty($token);
+
+        static::assertFalse($this->adapter->has('example')->wait());
+
+        $part = new FilePart(1, new Stream(static::asStream('hello ')));
+        $hash = $this->adapter->multipartUpload($object, $token, $part)->wait();
+        static::assertSame(hash('sha256', 'hello '), $hash);
+        $parts[] = new FilePart(1, null, $hash);
+
+        $part = new FilePart(42, new Stream(static::asStream('world!')));
+        $hash = $this->adapter->multipartUpload($object, $token, $part)->wait();
+        static::assertSame(hash('sha256', 'world!'), $hash);
+        $parts[] = new FilePart(42, null, $hash);
+
+        static::assertFalse($this->adapter->has('example')->wait());
+
+        $this->adapter->multipartFinalize($object, $token, ...$parts)->wait();
+
+        static::assertTrue($this->adapter->has('example')->wait());
+        $actual = $this->adapter->get('example')->wait();
+        static::assertSame('hello world!', $actual->data?->getContents());
+    }
+
+    /**
+     * Test {@see InMemoryAdapter::multipartUpload()} with an invalid token.
+     *
+     * @return void
+     * @covers ::multipartUpload()
+     */
+    public function testMultipartUploadNotInitialized(): void
+    {
+        $this->expectExceptionObject(new BadDataException('Multipart upload not initialized: EXAMPLE-TOKEN'));
+
+        $object = new FileObject('example', null);
+        $part = new FilePart(1, new Stream(static::asStream('hello ')));
+        $this->adapter->multipartUpload($object, 'EXAMPLE-TOKEN', $part)->wait();
+    }
+
+    /**
+     * Test {@see InMemoryAdapter::multipartFinalize()} with an invalid token.
+     *
+     * @return void
+     * @covers ::multipartFinalize()
+     */
+    public function testMultipartFinalizeNotInitialized(): void
+    {
+        $this->expectExceptionObject(new BadDataException('Multipart upload not initialized: EXAMPLE-TOKEN'));
+
+        $object = new FileObject('example', null);
+        $parts[] = new FilePart(1, null, hash('sha256', 'hello '));
+        $parts[] = new FilePart(42, null, hash('sha256', 'world!'));
+        $this->adapter->multipartFinalize($object, 'EXAMPLE-TOKEN', ...$parts)->wait();
+    }
+
+    /**
+     * Test {@see InMemoryAdapter::multipartFinalize()} with an invalid token.
+     *
+     * @return void
+     * @covers ::multipartFinalize()
+     * @uses \Chialab\ObjectStorage\InMemoryAdapter::multipartInit()
+     * @uses \Chialab\ObjectStorage\InMemoryAdapter::multipartUpload()
+     */
+    public function testMultipartFinalizeKeyMismatch(): void
+    {
+        $object = new FileObject('example', null);
+        $token = $this->adapter->multipartInit($object)->wait();
+
+        $part = new FilePart(1, new Stream(static::asStream('hello ')));
+        $parts[] = new FilePart(1, null, $this->adapter->multipartUpload($object, $token, $part)->wait());
+
+        $part = new FilePart(42, new Stream(static::asStream('world!')));
+        $parts[] = new FilePart(42, null, $this->adapter->multipartUpload($object, $token, $part)->wait());
+
+        $this->expectExceptionObject(new BadDataException('Multipart upload not initialized: ' . $token));
+        $this->adapter->multipartFinalize(new FileObject('ANOTHER-KEY', null), $token, ...$parts)->wait();
+    }
+
+    /**
+     * Test {@see InMemoryAdapter::multipartFinalize()} with parts in the incorrect order.
+     *
+     * @return void
+     * @covers ::multipartFinalize()
+     * @uses \Chialab\ObjectStorage\InMemoryAdapter::multipartInit()
+     * @uses \Chialab\ObjectStorage\InMemoryAdapter::multipartUpload()
+     */
+    public function testMultipartFinalizeUnsortedParts(): void
+    {
+        $object = new FileObject('example', null);
+        $token = $this->adapter->multipartInit($object)->wait();
+
+        $part = new FilePart(1, new Stream(static::asStream('hello ')));
+        $parts[] = new FilePart(1, null, $this->adapter->multipartUpload($object, $token, $part)->wait());
+
+        $part = new FilePart(42, new Stream(static::asStream('world!')));
+        $parts[] = new FilePart(42, null, $this->adapter->multipartUpload($object, $token, $part)->wait());
+
+        $this->expectExceptionObject(new BadDataException('Parts must be sorted monotonically'));
+        $this->adapter->multipartFinalize($object, $token, ...array_reverse($parts))->wait();
+    }
+
+    /**
+     * Test {@see InMemoryAdapter::multipartFinalize()} with a part whose hash does not match with the hash of the
+     * latest uploaded part for its index.
+     *
+     * @return void
+     * @covers ::multipartFinalize()
+     * @uses \Chialab\ObjectStorage\InMemoryAdapter::multipartInit()
+     * @uses \Chialab\ObjectStorage\InMemoryAdapter::multipartUpload()
+     */
+    public function testMultipartFinalizeHashMismatch(): void
+    {
+        $object = new FileObject('example', null);
+        $token = $this->adapter->multipartInit($object)->wait();
+
+        $part = new FilePart(1, new Stream(static::asStream('hello ')));
+        $hash = $this->adapter->multipartUpload($object, $token, $part)->wait();
+        $parts[] = new FilePart(1, null, $hash);
+
+        $part = new FilePart(42, new Stream(static::asStream('world!')));
+        $hash = $this->adapter->multipartUpload($object, $token, $part)->wait();
+        $parts[] = new FilePart(42, null, strrev($hash)); // Hash is reversed.
+
+        $this->expectExceptionObject(new BadDataException('Hash mismatch for part 42'));
+        $this->adapter->multipartFinalize($object, $token, ...$parts)->wait();
+    }
+
+    /**
+     * Test {@see InMemoryAdapter::multipartAbort()} method.
+     *
+     * @return void
+     * @covers ::multipartAbort()
+     * @uses \Chialab\ObjectStorage\InMemoryAdapter::multipartInit()
+     * @uses \Chialab\ObjectStorage\InMemoryAdapter::multipartUpload()
+     */
+    public function testMultipartAbort(): void
+    {
+        $object = new FileObject('example', null);
+        $part = new FilePart(1, new Stream(static::asStream('hello ')));
+
+        $token = $this->adapter->multipartInit($object)->wait();
+        $this->adapter->multipartUpload($object, $token, $part)->wait();
+
+        $this->adapter->multipartAbort($object, $token)->wait();
+
+        // Assert that an aborted multipart upload cannot be resumed:
+        static::assertThrows(
+            new BadDataException('Multipart upload not initialized: ' . $token),
+            fn () => $this->adapter->multipartUpload($object, $token, $part)->wait(),
+        );
+    }
+
+    /**
+     * Test {@see InMemoryAdapter::multipartAbort()} method with an invalid token.
+     *
+     * @return void
+     * @covers ::multipartAbort()
+     * @uses \Chialab\ObjectStorage\InMemoryAdapter::multipartUpload()
+     */
+    public function testMultipartAbortNotInitialized(): void
+    {
+        $token = 'EXAMPLE-TOKEN';
+        $object = new FileObject('example', null);
+        $part = new FilePart(1, new Stream(static::asStream('hello ')));
+
+        $this->adapter->multipartAbort($object, $token)->wait();
+
+        // Assert that an aborted multipart upload cannot be resumed:
+        static::assertThrows(
+            new BadDataException('Multipart upload not initialized: ' . $token),
+            fn () => $this->adapter->multipartUpload($object, $token, $part)->wait(),
+        );
+    }
+}
